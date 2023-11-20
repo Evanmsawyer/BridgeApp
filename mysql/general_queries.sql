5771d879fd1475ea6763798ac96464d5a73c4aeb
CjamesR10 <cjamesr10@gmail.com>
2023-11-19 21:02:30 -0500

    updated general_queries.sql

--- mysql/general_queries.sql
+++ mysql/general_queries.sql
@@ -72,11 +72,17 @@ FROM bridgedb.hands
 WHERE Hearts = '' OR Spades = '' OR Diamonds = '' OR Clubs = '';
 
 --List play where a game contract was bid and made >= 400 and the declarer+dummy had <= 25 HCP
+WITH Team_HCP(BoardID, Value, Position) AS
+   (SELECT h1.BoardID, h1.HighCardPoints + h2.HighCardPoints, h1.Position
+    FROM bridgedb.hands AS h1, bridgedb.hands AS h2
+    WHERE h1.BoardID = h2.BoardID AND ((h1.Position = 'N' AND h2.Position = 'S')
+      OR (h1.Position = 'E' AND h2.Position = 'W')) 
+      AND h1.HighCardPoints + h2.HighCardPoints <= 25)
 SELECT BoardID
-FROM bridgedb.hands
-NATURAL JOIN bridgedb.TableEntity
-WHERE TableEntity.RawScore >= 400 AND Player.HighCardPoints < 25;
-
+FROM Team_HCP
+NATURAL JOIN (SELECT * FROM bridgedb.TableEntity WHERE TableEntity.RawScore >= 400) AS t1
+WHERE ((Team_HCP.Position = 'N' AND SUBSTRING(t1.LastBid, 1, 1) IN ('S', 'N'))
+    OR (Team_HCP.Position = 'E' AND SUBSTRING(t1.LastBid, 1, 1) IN ('E', 'W')));
 
 --TOD:
 --List boards with a successful sacrifice
@@ -87,9 +93,3 @@ WHERE TableEntity.RawScore >= 400 AND Player.HighCardPoints < 25;
 --If they played dummy
 --Starting vs. Ending Score of Segments
 --Total Tournaments Won
-
-
-
-
-
-
