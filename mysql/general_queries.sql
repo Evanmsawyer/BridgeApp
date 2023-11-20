6befdcb6d7f6cd92cb96f8a09d3d453bb370c9a5
CjamesR10 <cjamesr10@gmail.com>
2023-11-19 20:10:51 -0500

    updated general_queries.sql, moved diff*

--- mysql/general_queries.sql
+++ mysql/general_queries.sql
@@ -44,42 +44,37 @@ SELECT * FROM Player WHERE Name = [Player Name];
 SELECT * FROM Team WHERE Name = [Team Name];
 
 --Complex Searches
---Search for all boards where slam was made
-SELECT Board.BoardID, TableEntity.FirstBid, TableEntity.LastBid
-FROM Board
-JOIN TableEntity ON Board.BoardID = TableEntity.BoardID
-WHERE TableEntity.LastBid IN ('6', '7') AND TableEntity.Result = 'Made';
+--Search for all boards where slam was bid and made
+SELECT TableEntity.BoardID, TableEntity.FirstBid, TableEntity.LastBid
+FROM bridgedb.TableEntity
+WHERE REGEXP_LIKE(TableEntity.Result, '^[6-7].[+=].*$');
 
 --List of boards where the same contract bid in both rooms but one team made it and the other did not
-SELECT b1.BoardID
-FROM TableEntity t1
-JOIN TableEntity t2 ON t1.PairedTableID = t2.TableID
-JOIN Board b1 ON t1.BoardID = b1.BoardID
-JOIN Board b2 ON t2.BoardID = b2.BoardID
-WHERE t1.LastBid = t2.LastBid AND t1.Result != t2.Result;
+SELECT t1.BoardID
+FROM bridgedb.TableEntity AS t1
+JOIN bridgedb.TableEntity AS t2 ON t1.PairedTableID = t2.TableID
+WHERE t1.LastBid = t2.LastBid AND t1.Result <> t2.Result;
 
---List of boars where the same contract bid in both rooms with different results and different opening lead
-SELECT b1.BoardID
-FROM TableEntity t1
-JOIN TableEntity t2 ON t1.PairedTableID = t2.TableID
-JOIN Board b1 ON t1.BoardID = b1.BoardID
-JOIN Board b2 ON t2.BoardID = b2.BoardID
-JOIN Trick tr1 ON t1.TableID = tr1.TableID
-JOIN Trick tr2 ON t2.TableID = tr2.TableID
-WHERE t1.LastBid = t2.LastBid AND t1.Result != t2.Result AND tr1.FirstSeat != tr2.FirstSeat;
+--List of boards where the same contract bid in both rooms with different results and different opening lead
+--Probably don't do this one - a less strict version of this search returned no results
+    --SELECT b1.BoardID
+    --FROM TableEntity AS t1
+    --JOIN TableEntity AS t2 ON t1.PairedTableID = t2.TableID
+    --JOIN Board b1 ON t1.BoardID = b1.BoardID
+    --JOIN Board b2 ON t2.BoardID = b2.BoardID
+    --JOIN Trick tr1 ON t1.TableID = tr1.TableID
+    --JOIN Trick tr2 ON t2.TableID = tr2.TableID
+    --WHERE t1.LastBid = t2.LastBid AND t1.Result != t2.Result AND tr1.FirstSeat != tr2.FirstSeat;
 
 --List boards where any hand had no cards of a given suit
-SELECT Board.BoardID
-FROM Hands
-JOIN Board ON Hands.Vulnerability = Board.Vulnerability
-WHERE (Hearts = '' OR Spades = '' OR Diamonds = '' OR Clubs = '');
+SELECT BoardID
+FROM bridgedb.hands
+WHERE Hearts = '' OR Spades = '' OR Diamonds = '' OR Clubs = '';
 
---List play where a game contract was bid and made >= 400 and the declarer+dummy had >= 25 HCP
-SELECT Board.BoardID
-FROM Board
-JOIN TableEntity ON Board.BoardID = TableEntity.BoardID
-JOIN PlaysTable ON TableEntity.TableID = PlaysTable.TableID
-JOIN Player ON PlaysTable.PlayerName = Player.Name
+--List play where a game contract was bid and made >= 400 and the declarer+dummy had <= 25 HCP
+SELECT BoardID
+FROM bridgedb.hands
+NATURAL JOIN bridgedb.TableEntity
 WHERE TableEntity.RawScore >= 400 AND Player.HighCardPoints < 25;
 
 
