bf4f5292f30b5a9fbf7cb3b9224ee82d3577dae3
Evanmsawyer <evan.m.sawyer@gmail.com>
2023-11-20 20:23:40 -0500

    added @

--- mysql/total_tricks_by_player_search.sql
+++ mysql/total_tricks_by_player_search.sql
@@ -2,7 +2,7 @@ SET DELIMITER $$
 CREATE PROCEDURE TotalTricksByPlayer(@PlayerNameInput VARCHAR(255), @TeamNameInput VARCHAR(255)) 
 AS BEGIN
     SELECT count(*)
-    FROM (SELECT TableID FROM bridgedb.PlaysTable WHERE PlayerName = @PlayerNameInput AND TeamName = TeamNameInput)
+    FROM (SELECT TableID FROM bridgedb.PlaysTable WHERE PlayerName = @PlayerNameInput AND TeamName = @TeamNameInput)
     NATURAL JOIN bridgedb.Trick
     WHERE Seat = WinningSeat
 END$$
