155f6d0badaf1ecc42db55f579897c62ec7b1302
a-carney <77558389+a-carney@users.noreply.github.com>
2023-12-02 16:32:18 -0500

    Update to dbCommunicator.py
    changes to OOP style and allow dynamic querying

--- app/dbCommunicator.py
+++ app/dbCommunicator.py
@@ -2,90 +2,126 @@ import mysql.connector
 from mysql.connector import Error
 import json
 
-def load_db_config(filename):
+class DBConnector:   
     """
-    Load database configuration from a JSON file
+    Class for connecting to a MySQL database
 
-    Args:
-        filename (str): The name of the JSON file containing the configuration
-
-    Returns:
-        dict: database configuration    
+    example:
+        db = DBConnector()
+        result = db.execute_query("SELECT * FROM table")
+        print(result) <-- this is a dict
+    
     """
-    with open(filename, 'r') as file:
-        return json.load(file)
+    def __init__(self):
+        self.db_config = self.load_db_config('../db_config.json')
+        self.connection = self.create_connection(self.db_config)
+    
+    def __del__(self):
+        self.close_connection(self.connection)
 
-db_config = load_db_config('../db_config.json')
+    def load_db_config(self, filename):
+        """
+        Load database configuration from a JSON file
 
-def create_connection():
-    """
-    creates DB connection
+        Args:
+            filename (str): The name of the JSON file containing the configuration
 
-    Returns:
-        connection: mysql.connector.connection_cext.CMySQLConnection object
-    Raises:
-        Error: If the connection cannot be established 
-    """
-    connection = None
-    try:
-        connection = mysql.connector.connect(**db_config)
-    except Error as e:
-        print(f"Error: '{e}'")
-    return connection
+        Returns:
+            dict: database configuration    
+        """
+        with open(filename, 'r') as file:
+            return json.load(file)
 
-def execute_query(query, connection, parameters=(), auto_commit=True):
-    """
-    Executes DB query
+    def create_connection(self, config):
+        """
+        creates DB connection
 
-    Args:
-        query (str): The query to be executed
-        parameters (tuple, optional): The parameters to be passed to the query. Defaults to ().
-        connection (mysql.connector.connection_cext.CMySQLConnection): The connection to the database
-        auto_commit (bool, optional): Whether to commit the query. Defaults to True.
-    Returns:
-        cursor: mysql.connector.cursor_cext.CMySQLCursor object        
-    Raises:
-        Error: If the query cannot be executed
+        Returns:
+            connection: mysql.connector.connection_cext.CMySQLConnection object
+        Raises:
+            Error: If the connection cannot be established 
+        """
+        try:
+            return mysql.connector.connect(**config)
+        except Error as e:
+            print(f"The error '{e}' occurred")
 
-    """
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query, parameters)
-        if auto_commit:
-            connection.commit()
-            print("Query executed successfully")
-        else:
-            return cursor
-    except Error as e:
-        print(f"The error '{e}' occurred")
+    def close_connection(self):
+        """
+        closes DB connection
 
-def execute_stored_procedure(procedure_name, connection, parameters=(), auto_commit=True):
-    """
-    Executes a stored proedure
+        Args:
+            connection (mysql.connector.connection_cext.CMySQLConnection): The connection to the database
+        Raises:
+            Error: If the connection cannot be closed 
+        """
+        try:
+            self.connection.close()
+        except Error as e:
+            print(f"The error '{e}' occurred")
 
-    Args:
-        procedure_name (str): The filename for the stored procedure 
-        parameters (tuple, optional): The parameters to be passed to the store procedure. Defaults to ().
-        connection (mysql.connector.connection_cext.CMySQLConnection): The connection to the database
-    Returns:
-        cursor: mysql.connector.cursor_cext.CMySQLCursor object        
-    Raises:
-        Error: If the store procedure cannot be executed
-    """
-    cursor = connection.cursor()
-    try:
-        cursor.callproc(procedure_name, parameters)
-        if auto_commit:
-            connection.commit()
-            print("Store procedure executed successfully")
-        else:
-            return cursor
-    except Error as e:
-        print(f"The error '{e}' occurred")
+    def execute_query(self, query, parameters=(), fetchall=True, num_rows=0):
+        """
+        Executes DB query
+
+        Args:
+            query (str): The query to be executed
+            parameters (tuple, optional): The parameters to be passed to the query. Defaults to ().
+            fetchall (bool, optional): Whether to fetch all results. Defaults to True.
+            num_rows (int, optional): The number of rows to fetch. Defaults to 0. (Used when fetchall=False)
+        Returns:
+            dict: The results of the query
+        Raises:
+            Error: If the query cannot be executed
+        """
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        if fetchall:
+            result = {}
+            for row in cursor.fetchall():
+                result[len(result)] = row
+            return result
+        else:   
+            result = {}
+            for row in cursor.fetchmany(num_rows):
+                result[len(result)] = row
+            return result
 
 
-# BEGIN TEST SCRIPT
-connection = create_connection()
-# print connection info
-print(connection)
+    def execute_query_as_stored_procedure(self, query, parameters=()):
+        """
+        Executes a query as a stored procedure
 
+        Args:
+            query (str): The query to be executed
+            connection (mysql.connector.connection_cext.CMySQLConnection): The connection to the database
+            parameters (tuple, optional): The parameters to be passed to the query. Defaults to ().
+        Returns:
+            dict: The results of the query
+        Raises:
+            Error: If the query cannot be executed
+        """
+        cursor = self.connection.cursor()
+        cursor.callproc(query, parameters)
+        
+    def execute_stored_procedure(self, procedure_name, parameters=(), auto_commit=True):
+        """
+        Executes a stored proedure
+
+        Args:
+            procedure_name (str): The filename for the stored procedure 
+            parameters (tuple, optional): The parameters to be passed to the store procedure. Defaults to ().
+            connection (mysql.connector.connection_cext.CMySQLConnection): The connection to the database
+            auto_commit (bool, optional): Whether to commit the query. Defaults to True.
+        Returns:
+            cursor: mysql.connector.cursor_cext.CMySQLCursor object        
+        Raises:
+            Error: If the store procedure cannot be executed
+        """
+        with self.connection.cursor() as cursor:
+            cursor.callproc(procedure_name, parameters)
+            if auto_commit:
+                self.connection.commit()
+            else:
+                return cursor.fetchall()
+                
