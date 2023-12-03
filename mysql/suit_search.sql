d0ee2da56c9993ce3a7eb1be34246e33f4f19ce8
Corey Reinholdtsen <cjamesr10@gmail.com>
2023-12-03 16:54:30 -0500

    Update suit_search.sql

--- mysql/suit_search.sql
+++ mysql/suit_search.sql
@@ -1,8 +1,8 @@
 SET DELIMITER $$
-CREATE PROCEDURE SuitSearch(@SpadesInput INT, @HeartsInput INT, @DiamondsInput INT, @ClubsInput INT)
-AS BEGIN
+CREATE PROCEDURE SuitSearch(SpadesInput INT, HeartsInput INT, DiamondsInput INT, ClubsInput INT)
+BEGIN
     SELECT * 
     FROM bridgedb.Hands 
-    WHERE LENGTH(Spades) = @SpadesInput AND LENGTH(Hearts) = @HeartsInput AND LENGTH(Diamonds) = @DiamondsInput AND LENGTH(Clubs) = @ClubsInput;
+    WHERE LENGTH(Spades) = SpadesInput AND LENGTH(Hearts) = HeartsInput AND LENGTH(Diamonds) = DiamondsInput AND LENGTH(Clubs) = ClubsInput;
 END$$
 SET DELIMITER ;
