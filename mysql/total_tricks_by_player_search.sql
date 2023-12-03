90fa2c4c568b3f4c5376fc4b7d6b07d11aa91078
Corey Reinholdtsen <cjamesr10@gmail.com>
2023-12-03 16:58:34 -0500

    Update total_tricks_by_player_search.sql

--- mysql/total_tricks_by_player_search.sql
+++ mysql/total_tricks_by_player_search.sql
@@ -1,9 +1,9 @@
 SET DELIMITER $$
-CREATE PROCEDURE TotalTricksByPlayer(@PlayerNameInput VARCHAR(255), @TeamNameInput VARCHAR(255)) 
-AS BEGIN
+CREATE PROCEDURE TotalTricksByPlayer(PlayerNameInput VARCHAR(255), TeamNameInput VARCHAR(255)) 
+BEGIN
     SELECT count(*)
-    FROM (SELECT TableID, Seat FROM bridgedb.PlaysTable WHERE PlayerName = @PlayerNameInput AND TeamName = @TeamNameInput) AS T
-    NATURAL JOIN bridgedb.Trick
+    FROM (SELECT TableID, Seat FROM BridgeDB.PlaysTable WHERE PlayerName = PlayerNameInput AND TeamName = TeamNameInput) AS T
+    NATURAL JOIN BridgeDB.Trick
     WHERE T.Seat = WinningSeat;
 END$$
 SET DELIMITER ;
