1ae638644cc61e413fb6487e35a608cbe2f9eaf1
Corey Reinholdtsen <cjamesr10@gmail.com>
2023-12-03 16:53:46 -0500

    Update startingbid_search.sql

--- mysql/startingbid_search.sql
+++ mysql/startingbid_search.sql
@@ -1,6 +1,6 @@
 SET DELIMITER $$
-CREATE PROCEDURE StartingBidSearch(@StartingBidInput VARCHAR(10)) 
-AS BEGIN
-    SELECT * FROM bridgedb.TableEntity WHERE SUBSTRING(FirstBid, 3, 2) = @StartingBidInput;
+CREATE PROCEDURE StartingBidSearch(StartingBidInput VARCHAR(10)) 
+BEGIN
+    SELECT * FROM BridgeDB.TableEntity WHERE SUBSTRING(FirstBid, 3, 2) = StartingBidInput;
 END$$
 SET DELIMITER ;
