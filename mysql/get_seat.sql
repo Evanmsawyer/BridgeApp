c90432696b82d8587209b7efe1866d40224947a4
Corey Reinholdtsen <cjamesr10@gmail.com>
2023-12-03 16:49:11 -0500

    Update get_seat.sql

--- mysql/get_seat.sql
+++ mysql/get_seat.sql
@@ -1,6 +1,6 @@
 SET DELIMITER $$
-CREATE PROCEDURE GetSeat(@TableIDInput INT, @PlayerNameInput varchar(255), @TeamNameInput varchar(255))
-AS BEGIN
-    SELECT Seat FROM bridgedb.PlaysTable WHERE TableID = @TableIDInput AND PlayerName = @PlayerNameInput AND TeamName = @TeamNameInput;
+CREATE PROCEDURE GetSeat(TableIDInput INT, PlayerNameInput varchar(255), TeamNameInput varchar(255))
+BEGIN
+    SELECT Seat FROM bridgedb.PlaysTable WHERE TableID = TableIDInput AND PlayerName = PlayerNameInput AND TeamName = TeamNameInput;
 END$$
 SET DELIMITER ;
