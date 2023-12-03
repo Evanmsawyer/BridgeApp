06ebf29e83737024417bc59fcbf3183802ec7fef
Corey Reinholdtsen <cjamesr10@gmail.com>
2023-12-03 16:41:43 -0500

    Update dealer_search.sql

--- mysql/dealer_search.sql
+++ mysql/dealer_search.sql
@@ -1,6 +1,6 @@
 SET DELIMITER $$
-CREATE PROCEDURE DealerSearch(@DealerInput char(1)) 
+CREATE PROCEDURE DealerSearch(DealerInput INT) 
 AS BEGIN
-    SELECT * FROM bridgedb.Board WHERE Dealer = @DealerInput;
+    SELECT * FROM bridgedb.Board WHERE Dealer = DealerInput;
 END$$
 SET DELIMITER ;
