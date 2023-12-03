2b061b06fdc0384ce4f841df675cefb5fe5b3e82
Corey Reinholdtsen <cjamesr10@gmail.com>
2023-12-03 16:51:36 -0500

    Update rawscore_search.sql

--- mysql/rawscore_search.sql
+++ mysql/rawscore_search.sql
@@ -1,6 +1,6 @@
 SET DELIMITER $$
-CREATE PROCEDURE RawScoreSearch(@RawScoreInputLow INT, @RawScoreInputHi INT) 
-AS BEGIN
-    SELECT * FROM bridgedb.TableEntity WHERE RawScore BETWEEN @RawScoreInputLow AND @RawScoreInputHi;
+CREATE PROCEDURE RawScoreSearch(RawScoreInputLow INT, RawScoreInputHi INT) 
+BEGIN
+    SELECT * FROM BridgeDB.TableEntity WHERE RawScore BETWEEN RawScoreInputLow AND RawScoreInputHi;
 END$$
 SET DELIMITER ;
